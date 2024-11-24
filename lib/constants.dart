e88178a671a38f7a7ee79595e929b19db80066bf
Даниил <45365913+chashkovdaniil@users.noreply.github.com>
2024-11-24 06:11:49 +0500

    Delete lib directory

--- lib/constants.dart
+++
@@ -1,1 +0,0 @@
-const baseDuration = Duration(milliseconds: 300);
--- lib/utils.dart
+++
@@ -1,9 +0,0 @@
-class Utils {
-  static String formatDate(DateTime? dateTime) {
-    final day = dateTime?.day.toString().padLeft(2, '0');
-    final month = dateTime?.month.toString().padLeft(2, '0');
-    final year = dateTime?.year.toString();
-
-    return '$day.$month.$year';
-  }
-}
